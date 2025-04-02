8a85b1dd4b3c2535590beced09910972c8775c90
hbybyyang <2451759073@qq.com>
2025-04-03 01:29:55 +0800

    增加处理没有协议的情况, 假设使用当前页面的域名

--- src/tools.ts
+++ src/tools.ts
@@ -1,4 +1,9 @@
 export function parseURL(url: string): { protocol: string; host: string; pathname: string } | null {
+  // 处理没有协议的情况，假设使用当前页面的域名
+  if (url.includes('://') === false) {
+    url = `${window.location.protocol}//${window.location.host}${url.startsWith('/') ? '' : '/'}${url}`
+  }
+
   let urlRegex = /^(https?:)\/\/(.*?)(\/.*|$)/
   let match = url.match(urlRegex)
 
